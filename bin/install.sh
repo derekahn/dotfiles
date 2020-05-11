702cc54b2e33728b7ccb9339e0f7c1e25dc4ba1e
derekahn <git.derek@gmail.com>
2020-05-11 09:56:40 -0700

    Manual install zsh theme vs npm

--- bin/install.sh
+++ bin/install.sh
@@ -113,7 +113,8 @@ echo "---------------------------------------------------------"
 echo "$(tput setaf 2)🏠: Installing spaceship prompt$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
-npm install -g spaceship-prompt
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)🏠: Installing bash language server$(tput sgr 0)"
