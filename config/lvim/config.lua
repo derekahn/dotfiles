85131c987552337c5dba2b01dea0e1799db2542c
derekahn <git.derek@gmail.com>
2022-10-25 11:47:58 -0700

    format config.lua

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -60,8 +60,6 @@ keymap("i", "<C-l>", "<Right>", opts) -- right one char
 -- keymap("i", "<C-j>", "<Down>", opts) -- up one line
 -- keymap("i", "<C-k>", "<Up>", opts) -- down one line
 
-
-
 -- Options
 vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.showtabline = 0 -- never display page tab labels (default 2)
@@ -91,67 +89,65 @@ lvim.builtin.which_key.mappings["u"] = { "<cmd>lua require('undotree').toggle()<
 lvim.builtin.which_key.setup.plugins.spelling.enabled = false
 
 lvim.builtin.which_key.mappings["r"] = {
-  name = "Rust",
-  i = { "<cmd>lua require('rust-tools').inlay_hints.set()<cr>", "Inlay Hints" },
-  r = { "<cmd>RustRun<cr>", "Run Buffer" },
-  a = { "<cmd>RustEmitAsm<cr>", "Show Assembly" },
-  R = { "<cmd>lua require('rust-tools').runnables.runnables()<cr>", "Runnables" },
-  e = { "<cmd>lua require('rust-tools').expand_macro.expand_macro()<cr>", "Expand Macros" },
-  o = { "<cmd>lua require('rust-tools').open_cargo_toml.open_cargo_toml()<cr>", "Open cargo.toml" },
-  p = { "<cmd>lua require('rust-tools').parent_module.parent_module()<cr>", "Open Parent Module" },
-  S = { "<cmd>RustStartStandaloneServerForBuffer<cr>", "Standalone Server" },
+	name = "Rust",
+	i = { "<cmd>lua require('rust-tools').inlay_hints.set()<cr>", "Inlay Hints" },
+	r = { "<cmd>RustRun<cr>", "Run Buffer" },
+	a = { "<cmd>RustEmitAsm<cr>", "Show Assembly" },
+	R = { "<cmd>lua require('rust-tools').runnables.runnables()<cr>", "Runnables" },
+	e = { "<cmd>lua require('rust-tools').expand_macro.expand_macro()<cr>", "Expand Macros" },
+	o = { "<cmd>lua require('rust-tools').open_cargo_toml.open_cargo_toml()<cr>", "Open cargo.toml" },
+	p = { "<cmd>lua require('rust-tools').parent_module.parent_module()<cr>", "Open Parent Module" },
+	S = { "<cmd>RustStartStandaloneServerForBuffer<cr>", "Standalone Server" },
 }
-
 
 -- https://github.com/nvim-telescope/telescope.nvim/issues/1923#issuecomment-1122642431
 local function getVisualSelection()
-  vim.cmd('noau normal! "vy"')
-  local text = vim.fn.getreg('v')
-  vim.fn.setreg('v', {})
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg("v")
+	vim.fn.setreg("v", {})
 
-  text = string.gsub(text, "\n", "")
-  if #text > 0 then
-    return text
-  else
-    return ''
-  end
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ""
+	end
 end
 
 function GrepSelectionInBuffer()
-  local input = getVisualSelection()
-  require("telescope.builtin").current_buffer_fuzzy_find({ default_text = input })
+	local input = getVisualSelection()
+	require("telescope.builtin").current_buffer_fuzzy_find({ default_text = input })
 end
 
 function GrepSelection()
-  local input = getVisualSelection()
-  require("telescope.builtin").live_grep({ default_text = input })
+	local input = getVisualSelection()
+	require("telescope.builtin").live_grep({ default_text = input })
 end
 
 function SpectreSelection()
-  local input = getVisualSelection()
-  require("spectre").open({ search_text = input })
+	local input = getVisualSelection()
+	require("spectre").open({ search_text = input })
 end
 
 lvim.builtin.which_key.vmappings["s"] = {
-  name = "Find and Replace",
-  s = {
-    "<cmd>lua GrepSelectionInBuffer()<cr>",
-    "Search Selection in Buffer"
-  },
-  S = {
-    "<cmd>lua GrepSelection()<cr>",
-    "Search Selection"
-  },
-  p = {
-    "<cmd>lua require('spectre').open_file_search()<cr>",
-    "Search & Replace in current buffer"
-  },
-  r = {
-    "<cmd>lua SpectreSelection()<cr>",
-    "Search & Replace"
-  }
+	name = "Find and Replace",
+	s = {
+		"<cmd>lua GrepSelectionInBuffer()<cr>",
+		"Search Selection in Buffer",
+	},
+	S = {
+		"<cmd>lua GrepSelection()<cr>",
+		"Search Selection",
+	},
+	p = {
+		"<cmd>lua require('spectre').open_file_search()<cr>",
+		"Search & Replace in current buffer",
+	},
+	r = {
+		"<cmd>lua SpectreSelection()<cr>",
+		"Search & Replace",
+	},
 }
-
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
@@ -164,219 +160,219 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 -- if you don't want all the parsers change this to a table of the ones you want
 -- Treesitter
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "css",
-  "go",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "rust",
-  "tsx",
-  "typescript",
-  "yaml",
+	"bash",
+	"css",
+	"go",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"rust",
+	"tsx",
+	"typescript",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = {
-  "haskell",
-  "java",
+	"haskell",
+	"java",
 }
 
 lvim.builtin.treesitter.highlight.enabled = true
 
-
 -- lualine
-local components = require "lvim.core.lualine.components"
+local components = require("lvim.core.lualine.components")
 
 lvim.builtin.lualine.sections = {
-  lualine_a = {
-    components.mode,
-  },
-  lualine_b = {
-    components.branch,
-    { "filename", path = 1 },
-  },
-  lualine_c = {
-    components.diff,
-  },
+	lualine_a = {
+		components.mode,
+	},
+	lualine_b = {
+		components.branch,
+		{ "filename", path = 1 },
+	},
+	lualine_c = {
+		components.diff,
+	},
 }
 
 -- Additional Plugins
 lvim.plugins = {
-  { "christoomey/vim-tmux-navigator" }, -- seamless navigation between tmux panes and vim splits
-  { "tpope/vim-surround" }, -- quoting/parenthesizing made simple (!lua)
-  { "tpope/vim-abolish" }, -- case coercion (!lua)
-  { "romgrk/nvim-treesitter-context" }, -- show code context
-  { "kevinhwang91/nvim-bqf", ft = "qf" }, -- better quickfix window in Neovim, polish old quickfix windo
+	{ "christoomey/vim-tmux-navigator" }, -- seamless navigation between tmux panes and vim splits
+	{ "tpope/vim-surround" }, -- quoting/parenthesizing made simple (!lua)
+	{ "tpope/vim-abolish" }, -- case coercion (!lua)
+	{ "romgrk/nvim-treesitter-context" }, -- show code context
+	{ "kevinhwang91/nvim-bqf", ft = "qf" }, -- better quickfix window in Neovim, polish old quickfix windo
 
-  { --  neovim undotree
-    "jiaoshijie/undotree",
-    config = function()
-      local ok, undotree = pcall(require, "undotree")
-      if ok then
-        undotree.setup()
-      end
-    end,
-  },
+	{ --  neovim undotree
+		"jiaoshijie/undotree",
+		config = function()
+			local ok, undotree = pcall(require, "undotree")
+			if ok then
+				undotree.setup()
+			end
+		end,
+	},
 
-  { -- a tree like view for symbols in Neovim using the Language Server Protocol
-    "simrat39/symbols-outline.nvim",
-    config = function()
-      require('symbols-outline').setup()
-    end
-  },
+	{ -- a tree like view for symbols in Neovim using the Language Server Protocol
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
 
-  { -- atom's one dark and light theme
-    "navarasu/onedark.nvim", config = function()
-      local ok, onedark = pcall(require, "onedark")
-      if ok then
-        onedark.setup {
-          style = "darker"
-        }
-        onedark.load()
-      end
-    end
-  },
+	{ -- atom's one dark and light theme
+		"navarasu/onedark.nvim",
+		config = function()
+			local ok, onedark = pcall(require, "onedark")
+			if ok then
+				onedark.setup({
+					style = "darker",
+				})
+				onedark.load()
+			end
+		end,
+	},
 
-  { -- highlight arguments' definitions and usages, using treesitter
-    "m-demare/hlargs.nvim",
-    config = function()
-      local ok, hlargs = pcall(require, "hlargs")
-      if ok then
-        hlargs.setup({
-          color = "#dd636e",
-          highlight = {},
-          excluded_filetypes = {},
-          paint_arg_declarations = true,
-          paint_arg_usages = true,
-          paint_catch_blocks = {
-            declarations = false,
-            usages = false,
-          },
-          extras = {
-            named_parameters = false,
-          },
-          hl_priority = 10000,
-        })
-      end
-    end,
-  },
+	{ -- highlight arguments' definitions and usages, using treesitter
+		"m-demare/hlargs.nvim",
+		config = function()
+			local ok, hlargs = pcall(require, "hlargs")
+			if ok then
+				hlargs.setup({
+					color = "#dd636e",
+					highlight = {},
+					excluded_filetypes = {},
+					paint_arg_declarations = true,
+					paint_arg_usages = true,
+					paint_catch_blocks = {
+						declarations = false,
+						usages = false,
+					},
+					extras = {
+						named_parameters = false,
+					},
+					hl_priority = 10000,
+				})
+			end
+		end,
+	},
 
-  { -- highlight, list and search todo comments in your projects
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      local ok, todo = pcall(require, "todo-comments")
-      if ok then
-        todo.setup()
-      end
-    end,
-  },
+	{ -- highlight, list and search todo comments in your projects
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			local ok, todo = pcall(require, "todo-comments")
+			if ok then
+				todo.setup()
+			end
+		end,
+	},
 
-  { -- highlight, list and search todo comments in your projects
-    "simrat39/rust-tools.nvim",
-    config = function()
-      local ok, rt = pcall(require, "rust-tools")
-      if ok then
-        rt.setup({
-          server = {
-            standalone = true,
-            ["rust-analyzer"] = {
-              checkOnSave = {
-                command = "clippy",
-              },
-            },
-          },
-        })
-      end
-    end,
-  },
+	{ -- highlight, list and search todo comments in your projects
+		"simrat39/rust-tools.nvim",
+		config = function()
+			local ok, rt = pcall(require, "rust-tools")
+			if ok then
+				rt.setup({
+					server = {
+						standalone = true,
+						["rust-analyzer"] = {
+							checkOnSave = {
+								command = "clippy",
+							},
+						},
+					},
+				})
+			end
+		end,
+	},
 
-  { -- find and replace
-    "windwp/nvim-spectre",
-    event = "BufRead",
-    config = function()
-      local ok, spectre = pcall(require, "spectre")
-      if ok then
-        spectre.setup()
-      end
-    end,
-  },
+	{ -- find and replace
+		"windwp/nvim-spectre",
+		event = "BufRead",
+		config = function()
+			local ok, spectre = pcall(require, "spectre")
+			if ok then
+				spectre.setup()
+			end
+		end,
+	},
 
-  { -- A Neovim Telescope extension to open your browser bookmarks right from the editor
-    "dhruvmanila/telescope-bookmarks.nvim",
-    requires = {
-      "kkharji/sqlite.lua",
-    },
-    config = function()
-      local ok, telescope = pcall(require, "telescope")
-      if ok then
-        telescope.setup({
-          extensions = {
-            fzf = {
-              fuzzy = true, -- false will only do exact matching
-              override_generic_sorter = true, -- override the generic sorter
-              override_file_sorter = true, -- override the file sorter
-              case_mode = "smart_case", -- "ignore_case" or "respect_case"
-            },
-            bookmarks = {
-              debug = false,
-              firefox_profile_name = "dev-edition-default",
-              selected_browser = "firefox",
-              url_open_command = "open",
-            }
-          }
-        })
-        telescope.load_extension("bookmarks")
-      end
-    end,
-  },
+	{ -- A Neovim Telescope extension to open your browser bookmarks right from the editor
+		"dhruvmanila/telescope-bookmarks.nvim",
+		requires = {
+			"kkharji/sqlite.lua",
+		},
+		config = function()
+			local ok, telescope = pcall(require, "telescope")
+			if ok then
+				telescope.setup({
+					extensions = {
+						fzf = {
+							fuzzy = true, -- false will only do exact matching
+							override_generic_sorter = true, -- override the generic sorter
+							override_file_sorter = true, -- override the file sorter
+							case_mode = "smart_case", -- "ignore_case" or "respect_case"
+						},
+						bookmarks = {
+							debug = false,
+							firefox_profile_name = "dev-edition-default",
+							selected_browser = "firefox",
+							url_open_command = "open",
+						},
+					},
+				})
+				telescope.load_extension("bookmarks")
+			end
+		end,
+	},
 
-  { -- orgmode for neovim
-    "nvim-neorg/neorg",
-    config = function()
-      local ok, neorg = pcall(require, "neorg")
-      if ok then
-        neorg.setup {
-          load = {
-            ["core.defaults"] = {},
-            ["core.norg.dirman"] = {
-              config = {
-                workspaces = {
-                  work = "~/notes/work",
-                  home = "~/notes/home",
-                }
-              }
-            }
-          }
-        }
-      end
-    end,
-    requires = "nvim-lua/plenary.nvim"
-  },
+	{ -- orgmode for neovim
+		"nvim-neorg/neorg",
+		config = function()
+			local ok, neorg = pcall(require, "neorg")
+			if ok then
+				neorg.setup({
+					load = {
+						["core.defaults"] = {},
+						["core.norg.dirman"] = {
+							config = {
+								workspaces = {
+									work = "~/notes/work",
+									home = "~/notes/home",
+								},
+							},
+						},
+					},
+				})
+			end
+		end,
+		requires = "nvim-lua/plenary.nvim",
+	},
 
-  { -- markdown preview plugin for (neo)vim
-    "iamcco/markdown-preview.nvim",
-    run = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
+	{ -- markdown preview plugin for (neo)vim
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 
-  { -- A light-weight lsp plugin based on neovim's built-in lsp with a highly performant UI
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    config = function()
-      local ok, saga = pcall(require, "lspsaga")
-      if ok then
-        saga.init_lsp_saga({
-          code_action_lightbulb = {
-            enable = false,
-            enable_in_insert = false,
-          },
-        })
-      end
-    end,
-  }
+	{ -- A light-weight lsp plugin based on neovim's built-in lsp with a highly performant UI
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		config = function()
+			local ok, saga = pcall(require, "lspsaga")
+			if ok then
+				saga.init_lsp_saga({
+					code_action_lightbulb = {
+						enable = false,
+						enable_in_insert = false,
+					},
+				})
+			end
+		end,
+	},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -384,29 +380,29 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("TextYankPost", {
-  group = augroup("HighlightYank", {}),
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch",
-      timeout = 40,
-    })
-  end,
+	group = augroup("HighlightYank", {}),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
 autocmd("BufEnter", {
-  pattern = { "*.json", "*.jsonc" },
-  -- enable wrap mode for json files only
-  command = "setlocal wrap",
+	pattern = { "*.json", "*.jsonc" },
+	-- enable wrap mode for json files only
+	command = "setlocal wrap",
 })
 
 -- Formatting
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-  {
-    command = "prettier",
-    filetypes = { "typescript", "typescriptreact", "json", "markdown" },
-  },
-  { command = "goimports", filetypes = { "go" } },
-  { command = "stylua", filetypes = { "lua" } },
+	{
+		command = "prettier",
+		filetypes = { "typescript", "typescriptreact", "json", "markdown" },
+	},
+	{ command = "goimports", filetypes = { "go" } },
+	{ command = "stylua", filetypes = { "lua" } },
 })
