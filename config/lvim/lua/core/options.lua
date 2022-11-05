2fe3c89e6619f82fa6bb4314607ac484321c6d30
derekahn <git.derek@gmail.com>
2022-11-04 17:26:14 -0700

    organize and optimize vim.opts

--- config/lvim/lua/core/options.lua
+++ config/lvim/lua/core/options.lua
@@ -1,5 +1,26 @@
-vim.opt.relativenumber = true -- set relative numbered lines
-vim.opt.showtabline = 0 -- never display page tab labels (default 2)
-vim.opt.timeoutlen = 200 -- determine the behavior when part of a key code sequence has been received by the (default 1000);
-vim.opt.updatetime = 50 -- shorten delay; (default is 4000 ms = 4 s) leads to noticeable (default 100)
-vim.opt.autoindent = true -- autoindents
+local opt = vim.opt
+
+-----------------------------------------------------------
+-- Neovim UI
+-----------------------------------------------------------
+opt.relativenumber = true -- show relative distance between rows
+opt.showtabline = 0 -- never display page tab labels (default 2)
+opt.syntax = "enable" -- enable syntax highlighting
+opt.colorcolumn = "99999" -- fixes indentline for now
+opt.list = true -- shows trailing indentions and spaces
+
+-----------------------------------------------------------
+-- Memory, CPU
+-----------------------------------------------------------
+opt.hidden = true -- enable background buffers
+opt.history = 100 -- remember n lines in history
+opt.lazyredraw = true -- faster scrolling
+opt.synmaxcol = 240 -- max column for syntax highlight
+opt.timeoutlen = 200 -- determine the behavior when part of a key code sequence has been received by the (default 1000);
+opt.updatetime = 50 -- shorten delay; (default is 4000 ms = 4 s) leads to noticeable (default 100)
+
+-----------------------------------------------------------
+-- Tabs, indent
+-----------------------------------------------------------
+opt.smartindent = true -- autoindent new lines
+opt.autoindent = true -- copy indent from current line
