a7d78131e6ac88799b6633fdbf1125258d249a21
derekahn <git.derek@gmail.com>
2022-11-05 08:35:16 -0700

    configure indentlines to show trees

--- config/lvim/lua/config/indentlines.lua
+++ config/lvim/lua/config/indentlines.lua
@@ -5,8 +5,9 @@ indentlines.active = true
 indentlines.options = {
 	enabled = true,
 	show_current_context = true,
-	show_current_context_start = true,
+	-- show_current_context_start = true,
 	show_first_indent_level = true,
 	space_char_blankline = " ",
 	use_treesitter = true,
+	use_treesitter_scope = true,
 }
