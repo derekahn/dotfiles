6410325b67d0cb411ceecffa1d6d8cbdbad50496
derekahn <git.derek@gmail.com>
2023-02-10 20:27:52 -0800

    remove lazy loading for plugins

--- config/lvim/lua/plugins.lua
+++ config/lvim/lua/plugins.lua
@@ -1,15 +1,15 @@
 lvim.plugins = {
-	{ "jose-elias-alvarez/typescript.nvim", lazy = true }, -- plugin to write TypeScript
-	{ "kevinhwang91/nvim-bqf", lazy = true, ft = "qf" }, -- better quickfix window in Neovim, polish old quickfix windo
-	{ "leoluz/nvim-dap-go", lazy = true }, -- extension for nvim-dap providing configurations for launching go debugger (delve) and debugging individual tests
-	{ "m-demare/hlargs.nvim", lazy = true }, -- highlight arguments' definitions and usages, using treesitter
-	{ "mxsdev/nvim-dap-vscode-js", lazy = true }, -- nvim-dap adapter for vscode-js-debug
-	{ "olexsmir/gopher.nvim", lazy = true }, -- plugin for easy golang development
-	{ "onsails/lspkind-nvim", lazy = true }, -- vscode-like pictograms for neovim lsp completion items
-	{ "romgrk/nvim-treesitter-context", lazy = true }, -- show code context
-	{ "simrat39/rust-tools.nvim", lazy = true }, -- tools for better development in rust using neovim's builtin lsp
-	{ "tzachar/cmp-tabnine", lazy = true, build = "./install.sh" }, -- tabnine plugin for hrsh7th/nvim-cmp
 	-- { "mfussenegger/nvim-dap-python" }, -- nvim-dap extension to test and debug python
+	{ "jose-elias-alvarez/typescript.nvim" }, -- plugin to write TypeScript
+	{ "kevinhwang91/nvim-bqf", ft = "qf" }, -- better quickfix window in Neovim, polish old quickfix windo
+	{ "leoluz/nvim-dap-go" }, -- extension for nvim-dap providing configurations for launching go debugger (delve) and debugging individual tests
+	{ "m-demare/hlargs.nvim" }, -- highlight arguments' definitions and usages, using treesitter
+	{ "mxsdev/nvim-dap-vscode-js" }, -- nvim-dap adapter for vscode-js-debug
+	{ "olexsmir/gopher.nvim" }, -- plugin for easy golang development
+	{ "onsails/lspkind-nvim" }, -- vscode-like pictograms for neovim lsp completion items
+	{ "romgrk/nvim-treesitter-context" }, -- show code context
+	{ "simrat39/rust-tools.nvim" }, -- tools for better development in rust using neovim's builtin lsp
+	{ "tzachar/cmp-tabnine", build = "./install.sh" }, -- tabnine plugin for hrsh7th/nvim-cmp
 
 	{ -- easy Neovim-Tmux navigation
 		"alexghergh/nvim-tmux-navigation",
@@ -33,7 +33,6 @@ lvim.plugins = {
 
 	{ -- add/change/delete surrounding delimiter pairs with ease
 		"kylechui/nvim-surround",
-		lazy = true,
 		version = "*",
 		config = function()
 			local ok, surround = pcall(require, "nvim-surround")
@@ -45,7 +44,6 @@ lvim.plugins = {
 
 	{ -- telescope extension to open browser bookmarks
 		"dhruvmanila/telescope-bookmarks.nvim",
-		lazy = true,
 		dependencies = "kkharji/sqlite.lua",
 		config = function()
 			-- FIX: This breaks on fresh install due to a race condition.
@@ -76,7 +74,6 @@ lvim.plugins = {
 
 	{ -- neovim undotree with git diff
 		"jiaoshijie/undotree",
-		lazy = true,
 		config = function()
 			local ok, undotree = pcall(require, "undotree")
 			if ok then
@@ -102,7 +99,6 @@ lvim.plugins = {
 
 	{ -- tree like view for symbols in Neovim using the Language Server Protocol
 		"simrat39/symbols-outline.nvim",
-		lazy = true,
 		config = function()
 			local ok, symbols_outline = pcall(require, "symbols-outline")
 			if ok then
@@ -113,7 +109,6 @@ lvim.plugins = {
 
 	{ -- highlight, list and search todo comments in your projects
 		"folke/todo-comments.nvim",
-		lazy = true,
 		config = function()
 			local ok, todo = pcall(require, "todo-comments")
 			if ok then
@@ -124,7 +119,6 @@ lvim.plugins = {
 
 	{ -- helps managing crates.io dependencies
 		"saecki/crates.nvim",
-		lazy = true,
 		event = { "BufRead Cargo.toml" },
 		dependencies = {
 			"nvim-lua/plenary.nvim",
@@ -139,7 +133,6 @@ lvim.plugins = {
 
 	{ -- find and replace in visual mode
 		"windwp/nvim-spectre",
-		lazy = true,
 		event = "BufRead",
 		config = function()
 			local ok, spectre = pcall(require, "spectre")
@@ -151,7 +144,6 @@ lvim.plugins = {
 
 	{ -- markdown preview
 		"iamcco/markdown-preview.nvim",
-		lazy = true,
 		build = function()
 			vim.fn["mkdp#util#install"]()
 		end,
@@ -159,7 +151,6 @@ lvim.plugins = {
 
 	{ -- highly performant ui for lsp things
 		"glepnir/lspsaga.nvim",
-		lazy = true,
 		event = "BufRead",
 		config = function()
 			local ok, saga = pcall(require, "lspsaga")
