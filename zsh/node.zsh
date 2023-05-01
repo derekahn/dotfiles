44aa32bf433a02c7110d5c1b9b87a9cf5390d750
derekahn <git.derek@gmail.com>
2023-05-01 12:00:38 -0700

    consolidate volta.zsh and node.zsh

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -3,6 +3,17 @@ export PATH="/usr/local/sbin:$PATH"
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
 
+# Enable pnpm https://docs.volta.sh/advanced/pnpm
+export VOLTA_FEATURE_PNPM=1
+
+alias vr='volta run'
+
 # pnpm
 export PNPM_HOME="~/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
+
+export PNPM_HOME="~/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
--- zsh/volta.zsh
+++
@@ -1,6 +0,0 @@
-export VOLTA_HOME=$HOME/.volta
-
-export PATH="/usr/local/sbin:$PATH"
-export PATH="$VOLTA_HOME/bin:$PATH"
-
-alias vr='volta run'
