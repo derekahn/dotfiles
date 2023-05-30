bcd9d96aedb22efcad05a43638fe8632b1a960b1
derekahn <git.derek@gmail.com>
2023-05-30 09:05:20 -0700

    add lspkind support for copilot

--- config/lvim/lua/config/lspkind.lua
+++ config/lvim/lua/config/lspkind.lua
@@ -13,6 +13,7 @@ if ok then
 			luasnip = "(Snippet)",
 			buffer = "(Buffer)",
 			tmux = "(TMUX)",
+			copilot = "(Copilot)",
 			treesitter = "(TreeSitter)",
 		},
 
@@ -30,6 +31,11 @@ if ok then
 				local max_width = lvim.builtin.cmp.formatting.max_width
 				if max_width ~= 0 and #vim_item.abbr > max_width then
 					vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. lvim.icons.ui.Ellipsis
+				end
+
+				if entry.source.name == "copilot" then
+					vim_item.kind = lvim.icons.git.Octoface
+					vim_item.kind_hl_group = "CmpItemKindCopilot"
 				end
 
 				if lvim.use_icons then
